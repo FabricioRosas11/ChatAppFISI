f8e92cdb1ba4891ac55200f1c5e4bdd9a6a2a997
Jorge Ramírez <jorgearmando.ramirez@unmsm.edu.pe>
2023-04-21 01:49:20 -0500

    v2

--- ChatApp/app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
+++ ChatApp/app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -11,8 +11,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+
+import com.example.chatapp.MainActivity;
 import com.example.chatapp.databinding.ActivitySignUpBinding;
 import com.example.chatapp.utilities.Constants;
+import com.example.chatapp.utilities.PreferenceManager;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.provider.MediaStore;
@@ -30,6 +33,7 @@ import java.util.HashMap;
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
+    private PreferenceManager preferenceManager;
     private String encodedImage;
 
     @Override
@@ -38,6 +42,7 @@ public class SignUpActivity extends AppCompatActivity {
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
 
         setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setListeners();
     }
 
@@ -50,7 +55,7 @@ public class SignUpActivity extends AppCompatActivity {
         });
         binding.layoutImage.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            Intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             pickImage.launch(intent);
         });
 
@@ -68,7 +73,22 @@ public class SignUpActivity extends AppCompatActivity {
         user.put(Constants.KEY_EMAIL,binding.inputEmail.getText().toString());
         user.put(Constants.KEY_PASSWORD,binding.inputPassword.getText().toString());
         user.put(Constants.KEY_IMAGE,encodedImage);
-        database
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    loading(false);
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+                    preferenceManager.putString(Constants.KEY_USER_ID,documentReference.getId());
+                    preferenceManager.putString(Constants.KEY_NAME,binding.inputName.getText().toString());
+                    preferenceManager.putString(Constants.KEY_IMAGE,encodedImage);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                })
+                .addOnFailureListener(exception -> {
+                    loading(false);
+                    showToast(exception.getMessage());
+                });
     }
 
     private String encodeImage(Bitmap bitmap){
@@ -103,11 +123,12 @@ public class SignUpActivity extends AppCompatActivity {
     );
 
     private Boolean isValidSignUpDetails(){
+        /*
         if(encodedImage == null){
             showToast("Select Profile Image");
-            return false;
-        }
-        else if(binding.inputName.getText().toString().trim().isEmpty()){
+            //return false;
+        }*/
+        if(binding.inputName.getText().toString().trim().isEmpty()){
             showToast("Enter name");
             return false;
         }
