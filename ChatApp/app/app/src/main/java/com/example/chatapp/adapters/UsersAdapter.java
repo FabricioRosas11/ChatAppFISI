bb1e37043ee2d7194a7a5d4513811dd6cb4bff28
Jorge Ramírez <jorgearmando.ramirez@unmsm.edu.pe>
2023-04-29 23:57:08 -0500

    V4

---
+++ ChatApp/app/app/src/main/java/com/example/chatapp/adapters/UsersAdapter.java
@@ -0,0 +1,63 @@
+package com.example.chatapp.adapters;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatapp.databinding.ItemContainerUserBinding;
+import com.example.chatapp.listeners.UserListener;
+import com.example.chatapp.models.User;
+
+import java.util.List;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHolder>{
+
+    private final List<User> users;
+    private final UserListener userListener;
+
+    public UsersAdapter(List users, UserListener userListener) {
+        this.users = users;
+        this.userListener = userListener;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemContainerUserBinding itemContainerUserBinding = ItemContainerUserBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new UserViewHolder(itemContainerUserBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UsersAdapter.UserViewHolder holder, int position) {
+        holder.setuserData(users.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class UserViewHolder extends RecyclerView.ViewHolder{
+
+        ItemContainerUserBinding binding;
+
+        UserViewHolder(ItemContainerUserBinding itemContainerUserBinding){
+            super(itemContainerUserBinding.getRoot());
+            binding = itemContainerUserBinding;
+        }
+
+        void setuserData(User user){
+            binding.textName.setText(user.name);
+            binding.textEmail.setText(user.email);
+            binding.getRoot().setOnClickListener(v -> userListener.onUserClicker(user));
+        }
+
+    }
+}
